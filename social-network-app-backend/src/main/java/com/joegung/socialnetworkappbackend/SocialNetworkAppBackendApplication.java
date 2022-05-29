8b63798f7b0cf492a34864f21aa1941869276c45
clear <joegung77@yahoo.com>
2022-05-29 19:00:23 -0400

    initial commit

---
+++ social-network-app-backend/src/main/java/com/joegung/socialnetworkappbackend/SocialNetworkAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joegung.socialnetworkappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkAppBackendApplication.class, args);
+	}
+
+}
---
+++ social-network-app-backend/src/test/java/com/joegung/socialnetworkappbackend/SocialNetworkAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joegung.socialnetworkappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
